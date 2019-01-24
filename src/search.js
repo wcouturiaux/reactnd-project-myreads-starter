0bf3906978ec1cf97b4b0cdf488d383638befcb2
Will C <w.l.couturiaux@gmail.com>
2019-01-23 20:53:42 -0500

    add file/framework for search component

---
+++ src/search.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+class search extends React.Component {
+  state = {
+    
+  }
+
+  render() {
+    <Link
+      to='/'
+        className='close-search'>
+          Close
+    </Link>
+    return (
+      <div>Book Search</div>
+    )
+  }
+}
+
+export  default search
