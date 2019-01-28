9e477c6a8555b811964e1a1e12a20624de1939bb
Will C <w.l.couturiaux@gmail.com>
2019-01-27 23:42:34 -0500

    Change button with on click to link

--- src/search.js
+++ src/search.js
@@ -12,7 +12,7 @@ class Search extends React.Component {
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+          <Link to='/' className="close-search">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
