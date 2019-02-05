75ac7d9ba10c7f7b8ec18b87acbf4981992c326a
Will C <w.l.couturiaux@gmail.com>
2019-02-04 21:32:18 -0500

    Change value to defaultValue for selection

--- src/book.js
+++ src/book.js
@@ -18,7 +18,7 @@ class Book extends React.Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
           <div className="book-shelf-changer">
-            <select value={this.state.currentShelf} onChange={this.changeShelf}>
+            <select defaultValue={this.state.currentShelf} onChange={this.changeShelf}>
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
