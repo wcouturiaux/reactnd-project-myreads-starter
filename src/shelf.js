569813ab05086f7838b259ffaa232c114cd63506
Will C <w.l.couturiaux@gmail.com>
2019-02-04 21:37:56 -0500

    Remove unnecessary state

--- src/shelf.js
+++ src/shelf.js
@@ -2,14 +2,11 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Book from './book'
 
-let booksOnShelf = [];
-
 class Shelf extends React.Component {
 
   render() {
 
     return (
-      booksOnShelf = this.props.books,
 
       <div>
         <Link to='/search' className='open-search'> Search Books </Link>
@@ -18,7 +15,7 @@ class Shelf extends React.Component {
             <h2 className="bookshelf-title">{this.props.title}</h2>
             <div className="bookshelf-books">
               <ol className="books-grid">
-                {booksOnShelf.filter(books=>books.shelf===this.props.shelf).map((book)=>(
+                {this.props.books.filter(books=>books.shelf===this.props.shelf).map((book)=>(
                 <div key={book.id}>
                   <Book title={book.title} authors={book.authors} img={book.imageLinks.smallThumbnail} changeShelf={this.props.changeShelf} book={book}/>
                 </div>
